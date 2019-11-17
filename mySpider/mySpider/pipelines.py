c8685a3315f8ad3271448a72ba6984e7fc8578be
pleasewhy <1943788269@qq.com>
2019-11-17 10:29:15 +0800

    1.0

--- mySpider/mySpider/pipelines.py
+++ mySpider/mySpider/pipelines.py
@@ -5,22 +5,30 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 from mySpider.WangYiYunTable import *
+# 将爬取到的数据存储到数据库
+# 注意：添加失败需要回滚不然会一直添加该错误信息
 class MyspiderPipeline(object):
     def process_item(self, item, spider):
         if not session.query(Singer).filter_by(singer_id=item["singer_id"]).first():
-            session.add(Singer(singer_id=item['singer_id'], name=item['singer_name']))
+            try:
+                session.add(Singer(singer_id=item['singer_id'], name=item['singer_name']))
+            except:
+                session.rollback()
 
         if not session.query(Song).filter_by(song_id=item["song_id"]).first():
-            session.add(Song(
-                        song_id=item["song_id"],
-                        song_name=item["song_name"],
-                        singer_id=item["singer_id"],
-                        singer_name=item["singer_name"],
-                        album_name=item["album_name"],
-                        total=item['total'],
-                        crawl_time=item['crawl_time'],
-                        time_modified=item['crawl_time'])
-                    )
+            try:
+                session.add(Song(
+                            song_id=item["song_id"],
+                            song_name=item["song_name"],
+                            singer_id=item["singer_id"],
+                            singer_name=item["singer_name"],
+                            album_name=item["album_name"],
+                            total=item['total'],
+                            crawl_time=item['crawl_time'],
+                            time_modified=item['crawl_time'])
+                        )
+            except:
+                session.rollback()
 
         try:
             for comment in item["hotComments"]:
@@ -41,4 +49,4 @@ class MyspiderPipeline(object):
             session.commit()
         except:
             session.rollback()
-        return None
+        return item
